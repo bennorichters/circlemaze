b7ff9464d8c69c04a281896465f3edb04bddcbeb
Benno Richters <b.richters@gmail.com>
2023-04-22 08:32:23 +0200

    refac test methods

--- src/svg/parse.rs
+++ src/svg/parse.rs
@@ -72,6 +72,32 @@ mod parse_tests {
 
     use super::CartesianCoord;
 
+    #[test]
+    fn test_parse() {
+        let path = vec![
+            Border {
+                border_type: BorderType::Arc,
+                start: CircleCoordinate { circle: 0, step: 0 },
+                length: 3,
+            },
+            Border {
+                border_type: BorderType::Line,
+                start: CircleCoordinate { circle: 0, step: 2 },
+                length: 1,
+            },
+        ];
+        let expected = DataHolder {
+            params: vec![
+                Param::Move((70., 50.)),
+                Param::Arc(20, 1, (33.81966, 61.755707)),
+                Param::Move((33.81966, 38.244293)),
+                Param::Line((17.63932, 26.48859)),
+            ],
+            index: 0,
+        };
+        parse(path, expected);
+    }
+
     const EPSILON: f64 = 0.00001;
 
     enum Param {
@@ -85,66 +111,44 @@ mod parse_tests {
         index: usize,
     }
 
+    impl DataHolder {
+        fn end(mut self) -> Self {
+            self.index += 1;
+            self
+        }
+    }
+
     impl Canvas for DataHolder {
-        fn move_to(mut self, coord: CartesianCoord) -> Self {
+        fn move_to(self, coord: CartesianCoord) -> Self {
             if let Param::Move(exp_coord) = self.params[self.index] {
                 assert!(abs_diff_eq!(coord.0, exp_coord.0, epsilon = EPSILON));
                 assert!(abs_diff_eq!(coord.1, exp_coord.1, epsilon = EPSILON));
 
-                self.index += 1;
-                return self;
+                return self.end();
             }
             panic!();
         }
 
-        fn draw_arc(mut self, radius: u32, long_arc_flag: u8, coord: CartesianCoord) -> Self {
+        fn draw_arc(self, radius: u32, long_arc_flag: u8, coord: CartesianCoord) -> Self {
             if let Param::Arc(exp_radius, exp_flag, exp_coord) = self.params[self.index] {
                 assert_eq!(radius, exp_radius);
                 assert_eq!(long_arc_flag, exp_flag);
                 assert!(abs_diff_eq!(coord.0, exp_coord.0, epsilon = EPSILON));
                 assert!(abs_diff_eq!(coord.1, exp_coord.1, epsilon = EPSILON));
 
-                self.index += 1;
-                return self;
+                return self.end();
             }
             panic!();
         }
 
-        fn draw_line(mut self, coord: CartesianCoord) -> Self {
+        fn draw_line(self, coord: CartesianCoord) -> Self {
             if let Param::Line(exp_coord) = self.params[self.index] {
                 assert!(abs_diff_eq!(coord.0, exp_coord.0, epsilon = EPSILON));
                 assert!(abs_diff_eq!(coord.1, exp_coord.1, epsilon = EPSILON));
 
-                self.index += 1;
-                return self;
+                return self.end();
             }
             panic!()
         }
-    }
-
-    #[test]
-    fn test_parse() {
-        let b0 = Border {
-            border_type: BorderType::Arc,
-            start: CircleCoordinate { circle: 0, step: 0 },
-            length: 3,
-        };
-        let b1 = Border {
-            border_type: BorderType::Line,
-            start: CircleCoordinate { circle: 0, step: 2 },
-            length: 1,
-        };
-        parse(
-            vec![b0, b1],
-            DataHolder {
-                params: vec![
-                    Param::Move((70., 50.)),
-                    Param::Arc(20, 1, (33.81966, 61.755707)),
-                    Param::Move((33.81966, 38.244293)),
-                    Param::Line((17.63932, 26.48859)),
-                ],
-                index: 0,
-            },
-        );
     }
 }
